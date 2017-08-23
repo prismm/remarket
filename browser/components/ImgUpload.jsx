aab8532df6de147b7285542ae34a01922044937c
Priya Murthy <prismm@gmail.com>
2017-08-23 18:43:00 -0400

    perfected image upload component

--- browser/components/ImgUpload.jsx
+++ browser/components/ImgUpload.jsx
@@ -13,32 +13,36 @@ const HOST = window.location.protocol.concat('//').concat(window.location.host);
 import {storeUploadedPhotos_dispatch} from '../actions/listing';
 import UploadedFileCard from './UploadedFileCard.jsx';
 
-//need typecheck and sizemax, error handling
-
 /*----------------------- ImgUpload Component ---------------------------*/
 
 class ImgUpload extends Component {
   constructor(props) {
     super(props);
-    this.state = { files: {}, photos: [] };
-    this.timeout = null;
+    this.state = {
+      file: null, //should hold the individual file being uploaded
+      files: {}, //stored here for UI purposes
+      photos: [], //stores photo objects with AWS S3 links for dispatch to database upon submit
+      error: null,
+      progress: null
+    };
+    this.timeout = null; //set by onUploadFinish to clear progress bar
+    this.upload = null; //set by setUpload function as reference for file input, preprocessing
 
-    //react-mdl methods
-    this.setUpload = this.setUpload.bind(this);
-    this.loadStart = this.loadStart.bind(this);
-    this.handleProgress = this.handleProgress.bind(this);
-    this.onLoad = this.onLoad.bind(this);
-    this.onUploadFinish = this.onUploadFinish.bind(this);
+    //upload lifecycle methods, in order
+    this.setUpload = this.setUpload.bind(this); //sets this.upload as ref for file input, proprocessing, aborting
+    this.loadStart = this.loadStart.bind(this); //sets this.state.error = null, and this.state.file (to what end?)
+    this.handleProgress = this.handleProgress.bind(this); //sets progress from S3Upload for progress bar
+    this.onLoad = this.onLoad.bind(this); //this.abort() if error or !image; checks totalSize > 8mb and if so this.abort(), else initiates S3Upload requests and adds file to this.state.files
+    this.onUploadFinish = this.onUploadFinish.bind(this); //creates photo obj for db dispatch using response from AWS S3, adds to this.state.photos array, sets progress to 100% then clears progress
 
-    //react-mdl abort methods
-    this.abort = this.abort.bind(this);
-    this.handleListClick = this.handleListClick.bind(this);
+    //react-mdl abort & stop methods
+    this.onError = this.onError.bind(this); //invoked by S3Upload if there is an error; passes error message to this.abort() to abort
+    this.abort = this.abort.bind(this); //aborts HTTP request to AWS S3, aborts upload of file ref for file input on UI, sets this.state({progress: null, file: null, error})
+    this.checkForError = this.checkForError.bind(this); // on react upload, the progress event can sometimes happen once more after the abort has been called -- so this just a sanity check
+    this.handleListClick = this.handleListClick.bind(this); // removes an uploaded file from this.state.files and this.state.photos if the close button is clicked
 
     //submit button handler
     this.publishPhotos = this.publishPhotos.bind(this);
-    this.photoIndexGenerator = this.photoIndexGenerator.bind(this);
-    this.getIndex = this.getIndex.bind(this);
-    this.index = 1;
   }
 
   componentWillUnmount() {
@@ -47,13 +51,15 @@ class ImgUpload extends Component {
     }
   }
 
-  setUpload(upload) {
+  setUpload(upload, next) {
+    console.log("HERE WE ARE TRYING")
+    if (!next) next = console.log;
     this.upload = upload;
-    this.props.preprocess(upload, console.error);
+    this.props.preprocess(upload, next);
   }
 
   loadStart(file) {
-    this.setState({ file });
+    this.setState({ file, error: null });
   }
 
   handleProgress(percent, message){
@@ -61,80 +67,58 @@ class ImgUpload extends Component {
     this.setState({ progress: percent });
   }
 
-  handleProgressForReact (file, progress) {
-    // The progress event can sometimes happen once more after the abort
-    // has been called. So this just a sanity check
-    if (this.state.file === file) this.setState({ progress });
-    this.props.onProgress(progress, 'PROGRESS')
+  checkForError (file, progress) {
+    // The progress event can sometimes happen once more after the abort has been called. So this just a sanity check
+    if (this.state.error) return this.abort(this.state.error);
+  }
+
+  onError(errorMessage){
+    this.props.onError(errorMessage);
+    return this.abort(errorMessage);
   }
 
   onLoad(file, uploadResult) {
     const { name, size, type } = file;
     //checks file type -- must be image
-    if (!(type.match(/image/))){
-      return this.abort();
+    if (this.state.error){
+      return this.abort(this.state.error);
+    } else if (!(type.match(/image/))) {
+      return this.abort('Sorry, you can only upload images.')
     } else {
-      this.setState({error: null});
-      this.myUploader = new S3Upload({
-        fileElement: file,
-        signingUrl: '/s3/sign',
-        accept: 'image/*',
-        preprocess: this.props.preprocess,
-        onProgress: this.handleProgress,
-        onFinishS3Put: this.onUploadFinish,
-        onError: this.props.onError,
-        signingUrlMethod: 'GET',
-        signingUrlWithCredentials: true,
-        uploadRequestHeaders: {'Access-Control-Allow-Origin': '*', 'x-amz-acl': 'public-read'},
-        contentDisposition: 'auto',
-        server: HOST,
-        scrubFilename: (filename) => filename.replace(/[^\w\d_\\.]+/ig, '')
-      });
-    }
-    const files = Object.assign({}, this.state.files);
-    files[name] = { name, type, size, uploadResult, index: this.index};
-    this.photoIndexGenerator();
-    //checks total size of upload < 8MB
-    let totalSize = 0;
-    Object.keys(files).map(function(key){totalSize += files[key].size});
-    if (totalSize > 8000000){
-      return this.abort();
-    } else {
-      this.setState({error: null})
-    }
-    this.setState({ files });
-    // return this.props.handleUpload(this.state.files);
-  }
-
-  abort() {
-    this.myUploader && this.myUploader.abortUpload();
-    if (this.upload) {
-      this.upload.abort();
-    }
-    this.setState({ file: null, progress: null, error: 'Sorry, files must be images and cannot exceed 8MB in total.' });
-  }
-
-  //you'll need to use this.state.file to capture some file info (like name) and save unto photo obj so we can filter out that photo on listClick
-  /*Removes an uploaded file if the close IconButton is clicked*/
-  handleListClick(event) {
-    let target = event.target;
-    while (target && target.parentNode) {
-      if (target.dataset.name) {
-        console.log('LIST CLICK TARGET', target, target.dataset, target.index, target.dataset.index, target.dataset.name)
-        const files = Object.assign({}, this.state.files);
-        delete files[target.dataset.name];
-        this.setState({ files });
-        return;
-      }
-      target = target.parentNode;
+        const files = Object.assign({}, this.state.files); //copies this.state.files
+        files[name] = { name, type, size, uploadResult}; //adds new file to files with name as key
+        //checks total size of upload < 8MB
+        let totalSize = 0;
+        Object.keys(files).map(function(key){totalSize += files[key].size});
+        if (totalSize > 8000000){
+          return this.abort('Sorry, the upload was stopped. Please note that files cannot exceed 8MB in total size.');
+        } else {
+          this.setState({ file, files }); //sets this.state.files to new obj which includes new file //SETS THIS.STATE.FILE     
+          this.myUploader = new S3Upload({ //initiates S3Upload requests
+            fileElement: file,
+            signingUrl: '/s3/sign',
+            accept: 'image/*',
+            preprocess: this.setUpload,
+            onProgress: this.handleProgress,
+            onFinishS3Put: this.onUploadFinish,
+            onError: this.onError,
+            signingUrlMethod: 'GET',
+            signingUrlWithCredentials: true,
+            uploadRequestHeaders: {'Access-Control-Allow-Origin': '*', 'x-amz-acl': 'public-read'},
+            contentDisposition: 'auto',
+            server: HOST,
+            scrubFilename: (filename) => filename.replace(/[^\w\d_\\.]+/ig, '')
+          });
+        }
     }
   }
 
-  onUploadFinish(photo) {
-    console.log('what is on this photo', photo)
+  onUploadFinish(photo, file) {
+    //creates photo obj for db using response from AWS S3, adds to this.state.photos array
     let newPhoto = {
       link: HOST.concat(photo.publicUrl),
-      listingId: 1 //this.props.currentListing.id
+      listingId: this.props.currentListing.id,
+      name: file.name
     };
     let currentPhotos = this.state.photos;
     currentPhotos.push(newPhoto);
@@ -142,43 +126,56 @@ class ImgUpload extends Component {
         photos: currentPhotos,
         progress: 100
     })
+    //sets progress to 100% and then times out after 1 sec so progress bar disappears
     this.timeout = setTimeout(() => {
       this.timeout = null;
       this.setState({ progress: null });
     }, 1000);
-    console.log("Upload finished: " + photo.publicUrl)
+    console.log('Upload finished: ' + photo.publicUrl)
   }
 
-  photoIndexGenerator() {
-    this.index++;
-    // console.log(this.index);
-    // return this.index;
+  abort(errorMsg) {
+    if (!errorMsg) errorMsg = 'Sorry, the upload was stopped. Please note that files must be images and cannot exceed 8MB in total size.';
+    this.myUploader && this.myUploader.abortUpload(); //aborts HTTP request to AWS S3
+    if (this.upload && this.upload.abort){
+      this.upload.abort();
+    } //aborts upload of file ref for file input on UI
+    this.setState({ file: null, progress: null, error: errorMsg }); //SETS THIS.STATE.FILE
   }
 
-  getIndex(){
-    return this.index;
+  handleListClick(event) {
+    let target = event.target;
+    while (target && target.parentNode) {
+      if (target.dataset.name) {
+        let targetName = target.dataset.name
+        //removes clicked file from this.state.files
+        const files = Object.assign({}, this.state.files);
+        delete files[targetName];
+        //removes clicked file from this.state.photos
+        const photos  = this.state.photos.filter(photo => photo.name !== targetName )
+        this.setState({ files, photos });
+        return;
+      }
+      target = target.parentNode;
+    }
   }
 
   publishPhotos(){
-    console.log('PHOTOS ON STATE', this.state.photos)
-    return this.props.upload({id: 1}, this.state.photos);
+    return this.props.upload(this.props.currentListing, this.state.photos);
   }
 
   render() {
-    console.log("THIS.STATE", this.state);
     const { files, progress } = this.state;
-    const cards = Object.keys(files).map(key => <UploadedFileCard key={key} index={this.getIndex()} file={files[key]} />);
+    const cards = Object.keys(files).map(key => <UploadedFileCard key={key} file={files[key]} />);
     let stats, submit;
     if (typeof progress === 'number') {
       stats = [
         <LinearProgress id="progress" key="progress" value={progress} />,
-        <Button raised key="abort" className="abort-upload" label="Stop Upload" onClick={this.abort} />,
+        <Button raised key="abort" className="abort-upload" label="Stop Upload" onClick={() => this.abort('Upload stopped.')} />,
       ];
     }
 
-    if (Object.keys(files).length > 0) {
-      submit = []
-    }
+    if (Object.keys(files).length > 0) submit = []
 
     return (
       <div className="uploaded-pics-container">
@@ -191,6 +188,7 @@ class ImgUpload extends Component {
           ref={this.setUpload}
           label="Select photos to upload"
           onLoadStart={this.loadStart}
+          onProgress={this.checkForError}
           onLoad={this.onLoad}
         />
         {this.state.error ? <p className="login-error">{this.state.error}</p> : null}
@@ -204,7 +202,7 @@ class ImgUpload extends Component {
         >
           {cards}
         </CSSTransitionGroup>
-        <Button raised primary label="Publish Photos" onClick={this.publishPhotos} className="md-cell--12 md-cell--right" />
+        {this.state.photos && this.state.photos.length ? <Button raised primary label="Publish Photos" onClick={this.publishPhotos} className="md-cell--12 md-cell--right" /> : null}
       </div>
     );
   }
@@ -212,7 +210,11 @@ class ImgUpload extends Component {
 
 ImgUpload.propTypes = {
   currentListing: PropTypes.object.isRequired,
-  upload: PropTypes.func.isRequired
+  upload: PropTypes.func.isRequired,
+  preprocess: PropTypes.func,
+  onProgress: PropTypes.func, 
+  onFinish: PropTypes.func,
+  onError: PropTypes.func
 };
 
 /*---------------------------------Container------------------------------------*/
@@ -226,15 +228,15 @@ const mapState = state => ({
       console.log('Upload progress: ' + percent + '% ' + message);
   },
   onFinish: function(signResult) {
-      console.log("Upload finished: " + signResult.publicUrl)
+      console.log('Upload finished: ' + signResult.publicUrl)
   },
   onError: function(message) {
-      console.log("Upload error: " + message);
+      console.log('Upload error: ' + message);
   }
 });
 
 const mapDispatch = dispatch => ({
-upload: (listing, photos) => dispatch(storeUploadedPhotos_dispatch(listing, photos))
+  upload: (listing, photos) => dispatch(storeUploadedPhotos_dispatch(listing, photos))
 });
 
 export default connect(mapState, mapDispatch)(ImgUpload);
