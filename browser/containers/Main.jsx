248357303ca997906f1ddeebbf03a8b8c3d60244
Priya Murthy <prismm@gmail.com>
2017-08-26 22:23:16 -0400

    styling main component

--- browser/containers/Main.jsx
+++ browser/containers/Main.jsx
@@ -4,18 +4,20 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { logout_dispatch } from '../actions/user';
+import Button from 'react-md/lib/Buttons/Button';
 
 /*------------------- Main component ----------------------*/
 const Main = ({ children, handleClick, loggedIn }) => {
   return (
     <div>
       <Link to="/"><img className="remarket-logo" src="/imgs/recycling-pink.png" alt="" height="40" width="40" /></Link><h1 className="site-header"><Link to="/">remarket</Link></h1>
+      <Button raised primary className="network-button network-button-nyu" label="NYU" />
+      <Button raised primary className="network-button network-button-columbia" label="Columbia" />
       { loggedIn ?
           <nav className="site-nav">
-            <Link className="site-nav-link" to="/home">Home</Link>
             <a className="site-nav-link" href="#" onClick={handleClick}>Logout</a>
-            <Link className="site-nav-link" to="/listings/post">New Post</Link>
             <Link className="site-nav-link" to="/account">My Account</Link>
+            <Link className="site-nav-link" to="/listings/post">New Post</Link>
           </nav> :
           <nav className="site-nav">
             <Link className="site-nav-link" method="login" to="/login">Login</Link>
