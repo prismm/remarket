515c65c0d4c8ecf15bd3657e55240c629e29822f
Priya Murthy <prismm@gmail.com>
2017-09-12 14:04:27 -0400

    modification to webpack config and added babel polyfill

---
+++ webpack.config.production.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+require('babel-polyfill');
+
+module.exports = {
+    entry: ['babel-polyfill', './browser/index.jsx'], // assumes your entry point is the index.js in the root of your project folder
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js' // assumes your bundle.js will also be in the root of your project folder
+    },
+    resolve: { extensions: ['.scss', '.css', '.js', '.json'] },
+    devtool: 'source-map',
+    module: {
+        rules: [{
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react', 'es2015'] // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
+                }
+            },
+            {
+                test: /\.scss?$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css?$/, //for the sake of react-toolbox
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: true,
+                            importLoaders: 1,
+                            localIdentName: '[name]--[local]--[hash:base64:8]'
+                        }
+                    },
+                    'postcss-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
+
+};
